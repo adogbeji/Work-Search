8b156fcb5486f30ac9a6d6c3f8150c5d4439f027
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-06-26 11:25:34 +0100

    Import ejs & use express.static middleware

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 require('dotenv').config({path: './config.env'});
 const express = require('express');
+const ejs = require('ejs');
 
 
 const app = express();
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(`${__dirname}/public`));
+app.use('/account', express.static(`${__dirname}/public`));
+
+
+// For Next Time: Require mongoose package & connect to Atlas cluster!
 
 
 const port = process.env.PORT;
