2a06522b95e2e2c2d8602c25fe4c3853c5533d41
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-07-21 14:51:58 +0100

    Finish Home Page Section B

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true, useCreat
 const User = require('./models/user-model');
 
 
-// For Next Time: Continue building Home Page middle section B
+// For Next Time: Start building Home Page middle section C!
 
 
 app.get('/', (req, res) => {
