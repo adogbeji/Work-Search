3bce660affd29135af232449ba97a9a2894200f3
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-09-02 17:34:23 +0100

    Add h4 text beneath fifth image of Home Page Section C

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true, useCreat
 const User = require('./models/user-model');
 
 
-// For Next Time: Add text beneath 5th image of Home Page Section C!
+// For Next Time: Finish styling text beneath 5th image of Home Page Section C!
 
 
 app.get('/', (req, res) => {
